7816d52cc8ffab71ef5096fa8755ca192bff86a7
Tatu Arvela <tatu.arvela@gmail.com>
2020-02-18 09:02:02 +0200

    Add estimated execution times

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import selectionSort from "./selectionSort.js";
 import bubbleSort from "./bubbleSort.js";
 import quicksort from "./quicksort.js";
 
+// Test values: 1000000, 1000
 const inputData = Array.from({ length: 100000 }, () =>
   Math.floor(Math.random() * 1000)
 );
@@ -16,32 +17,31 @@ const expectedData = Array.from(inputData).sort(numberComparison);
 
 const useSortingAlgorithm = id => {
   switch (id) {
-    case 0: // Timsort
+    case 0: // Timsort: ~25ms
       return (array) => array.sort(numberComparison);
-    case 1: // Merge sort
+    case 1: // Merge sort: ~40ms
       return mergeSort;
-    case 2: // Insertion sort
+    case 2: // Insertion sort: ~4000ms
       return insertionSort;
-    case 3: // Counting sort
+    case 3: // Counting sort: ~200ms
       return countingSort;
-    case 4: // Shellsort
+    case 4: // Shellsort: ~25ms
       return shellsort;
-    case 5: // Selection sort
+    case 5: // Selection sort: ~7000ms
       return selectionSort;
-    case 6: // Bubble sort
+    case 6: // Bubble sort: ~22000ms
       return bubbleSort;
-    case 7: // Quicksort
+    case 7: // Quicksort: ~32ms
       return quicksort;
   }
 };
 
 function main() {
   console.log(`
-
 // SORT ------------------------------------------------------------------------
 `);
   console.time("Execution time");
-  const sortedData = useSortingAlgorithm(4)(inputData);
+  const sortedData = useSortingAlgorithm(6)(inputData);
   console.timeEnd("Execution time");
   console.log(
     sortedData.join("") === expectedData.join("")
